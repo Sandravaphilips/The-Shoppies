4ed7e63ad228765515e5acfbcb70e40be1f93d04
Sandravaphilips <lacasandra1@gmail.com>
2020-09-06 01:07:09 -0700

    Imported relevant components and built the markup

--- the-shoppies/src/App.js
+++ the-shoppies/src/App.js
@@ -1,25 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios';
+import SearchTab from './components/SearchTab';
+import SearchResultsTab from './components/SearchResultsTab';
+import NominationsTab from './components/NominationsTab';
 import './App.css';
+import { Heading, Flex, ThemeProvider, CSSReset, Spinner } from '@chakra-ui/core';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResult, setSearchResult] = useState({});
+  const [movie, setMovie] = useState({});
+  const [isDisabled, setIsDisabled] = useState(false);
+
+  useEffect(() => {
+    Axios.get('http://www.omdbapi.com/?i=tt3896198&apikey=cfb96976')
+    .then(({data}) => {
+      setMovie({...data, isNominated: false})
+    })
+    .catch(err => console.log(err))
+  }, [])
+
+  const onChange = e => {
+    setSearchTerm(e.target.value)
+  }
+
+  const onNominate = e => {
+      e.preventDefault()
+      movie.isNominated =  true
+      setIsDisabled(!isDisabled)
+  }
+
+  const onRemove = e => {
+      e.preventDefault()
+      movie.isNominated = false;
+      setIsDisabled(!isDisabled)
+  }
+
+  if(!movie.Title) {
+    return(
+      <Spinner />
+    )
+  }
+
+  return (    
+    <ThemeProvider>
+      <CSSReset />
+      <div className="App">
+        <Heading>The Shoppies</Heading>
+        <SearchTab onChange={onChange} searchTerm={searchTerm} />
+        <Flex>
+          <SearchResultsTab isDisabled={isDisabled} onNominate={onNominate} movie={movie} setSearchResult={setSearchResult} searchTerm={searchTerm} searchResult={searchResult} />
+          <NominationsTab movie={movie} onRemove={onRemove} />
+        </Flex>
+      </div>
+    </ThemeProvider>
+    
   );
 }
 
